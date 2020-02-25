195221d961d20a9bcff36fdef2a335903404fcae
80prozent <80prozent@differentdesign.de>
2020-02-25 18:03:22 +0100

    update for ExternalInterfaceService

--- lib/factories/base/utilities/ExternalInterfaceService.ts
+++ lib/factories/base/utilities/ExternalInterfaceService.ts
@@ -8,21 +8,32 @@ export interface IExternalInterfaceService {
 	eval(expression): any;
 	call(request): any;
 	getId(): string;
+	callback:(functionName: string, args: any[]) => any;
 }
 
 export var ExternalInterfaceService: IExternalInterfaceService = {
 	enabled: false,
+	callback:null,
 	initJS(callback: (functionName: string, args: any[]) => any) {
+		this.callback=callback;
 		// ...
 	},
 	registerCallback(functionName: string) {
-		// ...
+		if(!window["flash"]){
+			window["flash"]={};
+		}
+		window["flash"][functionName]=(args)=>{
+			this.callback(functionName, args);
+		}
 	},
 	unregisterCallback(functionName: string) {
-		// ...
+		if(!window["flash"]){
+			window["flash"]={};
+		}
+		delete window["flash"][functionName];
 	},
 	eval(expression: string): any {
-		// ...
+		return window.eval(expression);
 	},
 	call(request: string): any {
 		// ...
