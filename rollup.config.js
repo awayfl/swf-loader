29a1a449ae320508a0df3639a716d5fd22277da3
Rob Bateman <rob.bateman@gmail.com>
2020-02-25 01:40:23 +0000

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -8,8 +8,26 @@ module.exports = {
 		name: 'AwayflSwfLoader',
 		sourcemap: true,
 		format: 'umd',
-		file: './bundle/awayfl-swf-loader.umd.js'
+		file: './bundle/awayfl-swf-loader.umd.js',
+		globals: {
+			'@awayjs/core': 'AwayjsCore',
+			'@awayjs/graphics': 'AwayjsGraphics',
+			'@awayjs/materials': 'AwayjsMaterials',
+			'@awayjs/renderer': 'AwayjsRenderer',
+			'@awayjs/scene': 'AwayjsScene',
+			'@awayjs/stage': 'AwayjsStage',
+			'@awayjs/swf-viewer': 'AwayjsSwfViewer',
+		},
 	},
+	external: [
+		'@awayjs/core',
+		'@awayjs/graphics',
+		'@awayjs/materials',
+		'@awayjs/renderer',
+		'@awayjs/scene',
+		'@awayjs/stage',
+		'@awayjs/swf-viewer',
+	],
 	plugins: [
 		nodeResolve({
 			jsnext: true,
