c77ebefcd27a60deef0011301dcd68554ef29ff0
80prozent <80prozent@differentdesign.de>
2020-07-13 14:52:01 +0200

    AVMTestHandler updates for testing

--- lib/AVMTestHandler.ts
+++ lib/AVMTestHandler.ts
@@ -1,4 +1,4 @@
-import { IInputRecorder } from "@awayjs/scene";
+import { IInputRecorder, MouseManager } from "@awayjs/scene";
 import { SWFFile } from './parsers/SWFFile';
 var hasshownServerError: boolean = false;
 function serverError(error: any) {
@@ -14,12 +14,13 @@ export enum TEST_MODE {
 }
 export class AVMTestHandler implements IInputRecorder {
 	public frames: IAVMTestFrame[];
+	public events: StringMap<any>;
 	public frameIdx: number = 0;
 	public swfInfo: ISWFTestInfo;
 	public config: IAVMTestConfig;
 	public reportedSWFInfos: boolean;
-	public snapshotCnt:number=0;
-	public snapShotsUploaded:number=0;
+	public snapshotCnt: number = 0;
+	public snapShotsUploaded: number = 0;
 	private _finished: boolean;
 	private _avmStage: any;
 
@@ -27,14 +28,17 @@ export class AVMTestHandler implements IInputRecorder {
 		this.config = config;
 		this._avmStage = avmStage;
 		this.frameIdx = 0;
+		this.events = {};
 		this.reportedSWFInfos = false;
 		this.frames = [{
 			messages: [],
 			frameIdx: this.frameIdx
 		}];
 		this._finished = false;
-		this.snapshotCnt=0;
+		this.snapshotCnt = 0;
 
+		if(this.config.recordtest)
+			MouseManager.inputRecorder = this;
 		//if(this.config.mode==TEST_MODE.RECORDING){
 		//	if recording, we must have a way to stop the recording,
 		//	so we listen for a keyboard sortcut here on window
@@ -50,7 +54,7 @@ export class AVMTestHandler implements IInputRecorder {
 	 * @param message 
 	 */
 	public addMessage(message: string) {
-		if(this._finished)
+		if (this._finished)
 			return;
 		this.frames[this.frames.length - 1].messages.push(message);
 		this.checkIfFinished();
@@ -66,32 +70,123 @@ export class AVMTestHandler implements IInputRecorder {
 		};
 	}
 
-	public recordEvent() {
+	public recordEvent(event: any) {
+
+		if(this.config.settings.snapShotOnMouseDown && event.type=="mousedown")
+			this.takeSnapshot();
+			
+		if(this.config.settings.snapShotOnMouseUP && event.type=="mouseup")
+			this.takeSnapshot();
+
+		if(!this.config.recordtest)
+			return;
+		// todo allow to filter out all mousemove commands
+
+		if (!this.config.settings.recordInput)
+			return;
+
+		//if(event.type==)
+
+		if (!this.events[this.frameIdx])
+			this.events[this.frameIdx] = [];
+
+		let jsonEvent = {
+			type: event.type,
+			screenX: event.screenX,
+			screenY: event.screenY,
+			clientX: event.clientX,
+			clientY: event.clientY,
+			ctrlKey: event.ctrlKey,
+			altKey: event.altKey,
+			shiftKey: event.shiftKey
+		}
+
+		this.events[this.frameIdx].push(jsonEvent);
 	}
 
-	public closeBrowserTab() {
-		if(!this._finished)
+
+	public dispatchEvents() {
+		if(this.config.recordtest)
 			return;
-		if((this.snapShotsUploaded==0 && this.snapshotCnt==0) || this.snapShotsUploaded==this.snapshotCnt-1){
+		if (this.config.events && this.config.events[this.frameIdx]) {
+			for (let i = 0; i < this.config.events[this.frameIdx].length; i++) {
+				let jsonEvent = this.config.events[this.frameIdx][i];
+				jsonEvent.preventDefault = () => { };
+				switch (jsonEvent.type) {
+					case "click":
+						MouseManager.getInstance(null).onClick(jsonEvent);
+						break;
+					case "dblclick":
+						MouseManager.getInstance(null).onDoubleClick(jsonEvent);
+						break;
+					case "touchstart":
+						MouseManager.getInstance(null).onMouseDown(jsonEvent);
+						break;
+					case "mousedown":
+						if(this.config.settings.snapShotOnMouseDown)
+							this.takeSnapshot();			
+						MouseManager.getInstance(null).onMouseDown(jsonEvent);
+						break;
+					case "touchmove":
+						MouseManager.getInstance(null).onMouseMove(jsonEvent);
+						break;
+					case "mousemove":
+						MouseManager.getInstance(null).onMouseMove(jsonEvent);
+						break;
+					case "mouseup":
+						if(this.config.settings.snapShotOnMouseUP)
+							this.takeSnapshot();
+						MouseManager.getInstance(null).onMouseUp(jsonEvent);
+						break;
+					case "touchend":
+						MouseManager.getInstance(null).onMouseUp(jsonEvent);
+						break;
+					case "touchend":
+						MouseManager.getInstance(null).onClick(jsonEvent);
+						break;
+					case "mousewheel":
+						MouseManager.getInstance(null).onMouseWheel(jsonEvent);
+						break;
+					case "mouseover":
+						MouseManager.getInstance(null).onMouseOver(jsonEvent);
+						break;
+					case "mouseout":
+						MouseManager.getInstance(null).onMouseOut(jsonEvent);
+						break;
+					case "keydown":
+						MouseManager.getInstance(null).onKeyDown(jsonEvent);
+						break;
+					case "keyup":
+						MouseManager.getInstance(null).onKeyUp(jsonEvent);
+						break;
+						
+				}
+			}
+		}
+	}
+	public closeBrowserTab() {
+		if (!this._finished)
+			return;
+		if ((this.snapShotsUploaded == 0 && this.snapshotCnt == 0) || this.snapShotsUploaded == this.snapshotCnt) {
 			this.finishAndUploadTest();
 		}
 	}
 	public takeSnapshot() {
-		if(this._finished)
+		if (this._finished)
 			return;
-		let myThis=this;
-		let snapShotFrame=this.snapshotCnt++;
-		this.addMessage("AWAYFLTEST SNAPSHOT "+ snapShotFrame);
+		let myThis = this;
+		let snapShotFrame = this.snapshotCnt++;
+		this.addMessage("AWAYFLTEST SNAPSHOT " + snapShotFrame);
 		this._avmStage.snapshot(htmlCanvas => {
-			
-			htmlCanvas.toBlob((blob)=>onBlob(blob));
-			function onBlob (blob) {
+
+			htmlCanvas.toBlob((blob) => onBlob(blob));
+			function onBlob(blob) {
 				console.log("snapshot done");
 
 				const formData = new FormData();
-				var fileName ="snapshot_"+snapShotFrame+".png";
+				var fileName = "snapshot_" + snapShotFrame + ".png";
 				formData.append("file", blob, fileName);
-				
+
 				const request = new XMLHttpRequest();
 				request.onreadystatechange = function (oEvent) {
 					if (request.readyState == 4) {
@@ -122,24 +217,26 @@ export class AVMTestHandler implements IInputRecorder {
 	 * called from onEnter on stage
 	 */
 	public nextFrame() {
-		console.log("AVMTestHandler - showNextFrame");
 		this.frameIdx++;
-		if(this.config.settings.stopRecAfterFrame>0 && this.frameIdx>=this.config.settings.stopRecAfterFrame){
+		if (this.config.settings.stopRecAfterFrame > 0 && this.frameIdx > this.config.settings.stopRecAfterFrame) {
 			this.finishTest();
 		}
-		if (this.frames[this.frames.length - 1].messages.length > 0) {
-			this.frames.push({
-				messages: [],
-				frameIdx: this.frameIdx,
-			});
-		}
-		else {
-			this.frames[this.frames.length - 1].frameIdx = this.frameIdx;
+		if (this._finished)
+			return;
+		if (this.config.settings.snapShotFrames != 0 && this.frameIdx % this.config.settings.snapShotFrames == 0)
+			this.takeSnapshot();
+		if (!this.config.settings.onlyTraces) {
+			if (this.frames[this.frames.length - 1].messages.length > 0) {
+				this.frames.push({
+					messages: [],
+					frameIdx: this.frameIdx,
+				});
+			}
+			else {
+				this.frames[this.frames.length - 1].frameIdx = this.frameIdx;
+			}
 		}
 
-		if (this.config.mode == TEST_MODE.TESTING && this.config.loadedFrames.length < this.frames.length) {
-			this.finishTest();
-		}
 	}
 
 	public checkIfFinished() {
@@ -176,12 +273,12 @@ export class AVMTestHandler implements IInputRecorder {
 		this._finished = true;
 		this.closeBrowserTab();
 	}
-	
+
 	public finishAndUploadTest() {
 		if (this.frames[this.frames.length - 1].messages.length == 0) {
 			this.frames.pop();
 		}
-		let myThis=this;
+		let myThis = this;
 		var path = window.location.pathname;
 		var page = path.split("/").pop().replace(".html", "");
 		const data = {
@@ -191,8 +288,9 @@ export class AVMTestHandler implements IInputRecorder {
 			url: page,
 			swf: this.config.swfPath,
 			swfInfos: this.swfInfo,
-			settings: this.config,
+			settings: this.config.settings,
 			frames: this.frames,
+			events: this.events,
 			seed: this.config.seed,
 		}
 		const json = JSON.stringify(data);
@@ -212,7 +310,7 @@ export class AVMTestHandler implements IInputRecorder {
 			if (request.readyState == 4) {
 				if (request.status == 200 || request.status == 0) {
 					console.log("AWAYFLTEST END");
-					window.close();
+					//window.close();
 					//console.log(request.responseText)
 				} else {
 					serverError(request.statusText + "-" + request.readyState + "-" + request.responseText + "-" + request.status);
