abf6084e2762eb09ba33c2a3e19b4966d5ea4cea
Konstantin Timoshenko <rondo.devil@gmail.com>
2021-11-07 16:32:44 +0300

    fix: const enums exports

--- lib/factories/base/AVMVersion.ts
+++ lib/factories/base/AVMVersion.ts
@@ -1,5 +1,8 @@
-
-export const enum AVMVERSION {
-	AVM1='AVM1',
-	AVM2='AVM2'
+/**
+ * NOTE! Not use a const literals of exported from index enums,
+ * const enums is constantly replaced in compile time and really not exist in lib
+ */
+export enum AVMVERSION {
+	AVM1 = 'AVM1',
+	AVM2 = 'AVM2'
 }
