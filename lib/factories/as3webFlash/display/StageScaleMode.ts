32bbbac611a18b0b7e6993810891c0767a401ede
Rob Bateman <rob@infiniteturtles.co.uk>
2023-11-13 12:55:04 +0000

    correct EXACT_FIT string to 'exactFit'

--- lib/factories/as3webFlash/display/StageScaleMode.ts
+++ lib/factories/as3webFlash/display/StageScaleMode.ts
@@ -1,5 +1,5 @@
 export class StageScaleMode {
-	public static EXACT_FIT: string = 'EXACT_FIT';
+	public static EXACT_FIT: string = 'exactFit';
 	public static NO_BORDER: string = 'noBorder';
 	public static NO_SCALE: string = 'noScale';
 	public static SHOW_ALL: string = 'showAll';
